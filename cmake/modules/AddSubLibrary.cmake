fe46ea5f2bc452f665a3455962ddd454c4789d71
Jakob Wagner <wobakjerna@web.de>
2016-04-28 18:52:18 +0200

    add install option, linking with dynamic libs outside usr/lib doesnt work after install

---
+++ cmake/modules/AddSubLibrary.cmake
@@ -0,0 +1,43 @@
+# general script to create helper library from folder
+##############################################################################
+#input variables
+# LIB - library name
+# LIB_FOLDER - library folder
+# LIB_FLAGS - compilation flags
+##############################################################################
+#output variables
+#LIB_INCLUDE_DIRS
+#LIB_LIBRARIES
+##############################################################################
+cmake_minimum_required(VERSION 2.8)
+
+MACRO(add_sublibrary LIB LIB_FOLDER LIB_FLAGS)
+# collect source files
+file(GLOB ${LIB}_SOURCES ${LIB_FOLDER}/*.cpp ${LIB_FOLDER}/*.c)
+file(GLOB ${LIB}_HEADERS ${LIB_FOLDER}/*.hpp ${LIB_FOLDER}/*.h)
+
+# output an library
+add_library(lib${LIB} STATIC ${${LIB}_SOURCES})
+# set header files for install
+set_target_properties(
+    lib${LIB} PROPERTIES
+    PUBLIC_HEADER "${${LIB}_HEADERS}"
+    COMPILE_FLAGS "${LIB_FLAGS}"
+)
+# target_compile_definitions(lib${LIB} "-O3 -funroll-loops -fomit-frame-pointer -fPIC -msse2 -DDXT_INTR")
+# add includes so they get added automatically to targets depending on this
+target_include_directories(lib${LIB} SYSTEM PUBLIC ${LIB_FOLDER})
+
+#set output variables for linking
+string(TOUPPER ${LIB} LIB_CAPS)
+set(${LIB_CAPS}_LIBRARIES lib${LIB} CACHE STRING " librarie filnames")
+# set(${LIB_CAPS}_INCLUDE_DIRS ${LIB_FOLDER} CACHE STRING "include directories")
+
+INSTALL(
+    TARGETS lib${LIB}
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib
+    PUBLIC_HEADER DESTINATION include/${LIB}
+)
+
+ENDMACRO(add_sublibrary)
