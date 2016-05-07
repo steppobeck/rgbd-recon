b7d3c8ea6d2bba480c57e52499ff5d75c5aea472
wobakj <jakob.wagner@uni-weimar.de>
2016-05-08 01:11:54 +0200

    upload both original and inverted volumes to calib_vis shader, inversion result points should lie in unit cube

--- framework/reconstruction/recon_calibs.cpp
+++ framework/reconstruction/recon_calibs.cpp
@@ -27,10 +27,10 @@ ReconCalibs::ReconCalibs(CalibrationFiles const& cfs, CalibVolume const* cv, glo
     globjects::Shader::fromFile(GL_VERTEX_SHADER,   "glsl/calib_vis.vs"),
     globjects::Shader::fromFile(GL_FRAGMENT_SHADER, "glsl/calib_vis.fs")
   );
-  m_program->setUniform("cv_xyz", getXYZVolumeUnits());
-  m_program->setUniform("cv_uv", getUVVolumeUnits());
-  // m_program->setUniform("cv_xyz", m_cv->getXYZVolumeUnits());
-  // m_program->setUniform("cv_uv", m_cv->getUVVolumeUnits());
+  m_program->setUniform("cv_xyz2", getXYZVolumeUnits());
+  m_program->setUniform("cv_uv2", getUVVolumeUnits());
+  m_program->setUniform("cv_xyz", m_cv->getXYZVolumeUnits());
+  m_program->setUniform("cv_uv", m_cv->getUVVolumeUnits());
 
   m_program_sample->attach(
     globjects::Shader::fromFile(GL_VERTEX_SHADER,   "glsl/calib_sample.vs"),
@@ -41,9 +41,7 @@ ReconCalibs::ReconCalibs(CalibrationFiles const& cfs, CalibVolume const* cv, glo
   m_program_sample->setUniform("volume_xyz", start_image_unit);
   m_program_sample->setUniform("volume_uv", start_image_unit + 1);
 
-  // int num_slots =  globjects::getInteger(GL_MAX_IMAGE_UNITS);
-  // std::cout << num_slots << "slots!" << std::endl;
-  std::vector<float> empty_xyz(volume_res.x * volume_res.y * volume_res.z * 4, -1.0f);
+  std::vector<float> empty_xyz(volume_res.x * volume_res.y * volume_res.z * 4, -0.01f);
   std::vector<uv> empty_uv(volume_res.x * volume_res.y * volume_res.z, {-1.0f, -1.0f});
   for (unsigned i = 0; i < cfs.num(); ++i) {
     auto volume_xyz = globjects::Texture::createDefault(GL_TEXTURE_3D);
@@ -73,17 +71,6 @@ void ReconCalibs::draw(){
   //   m_program->setUniform("layer", i);
     m_sampler.sample();
   // }
-    // glBegin(GL_POINTS);
-    //   glVertex3f(0.0f, 0.0f, 0.0f);
-    //   glVertex3f(1.0f, 0.0f, 0.0f);
-    //   glVertex3f(0.0f, 1.0f, 0.0f);
-    //   glVertex3f(0.0f, 0.0f, 1.0f);
-
-    //   glVertex3f(1.0f, 1.0f, 0.0f);
-    //   glVertex3f(1.0f, 1.0f, 1.0f);
-    //   glVertex3f(0.0f, 1.0f, 1.0f);
-    //   glVertex3f(1.0f, 0.0f, 1.0f);
-    // glEnd();
 
   m_program->release();
 }
@@ -116,12 +103,12 @@ void ReconCalibs::process(){
   glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
   std::vector<float> xyz_data(volume_res.x * volume_res.y * volume_res.z * 4);
   m_volumes_xyz[0]->getImage(0, GL_RGBA, GL_FLOAT, xyz_data.data());
-  for(unsigned i = 0; i < xyz_data.size() / 4; ++i) {
-    if(xyz_data[i] > 0.0f) {
-      std::cout << "value " << xyz_data[i] << ", " << xyz_data[i + 1] << ", "<< xyz_data[i + 2] << ", " << xyz_data[i + 3] << " at " << i << std::endl;
-      break;
+  for(unsigned i = 0; i < xyz_data.size(); i+=4) {
+    if(xyz_data[i] > 1.0f || xyz_data[i + 1] > 1.0f || xyz_data[i + 2] > 1.0f
+    || xyz_data[i] < -0.01f || xyz_data[i + 1] < -0.01f || xyz_data[i + 2] < -0.01f) {
+      std::cout << "value " << xyz_data[i] << ", " << xyz_data[i + 1] << ", "<< xyz_data[i + 2] << " at " << i << std::endl;
+      throw std::exception{};
     } 
-    // std::cout << xyz_data[i * 3] << ", " <<xyz_data[i * 3 + 1] << "," << xyz_data[i * 3 + 2] << std::endl;
   }
 
   bindToTextureUnits();
