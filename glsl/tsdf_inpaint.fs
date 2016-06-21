18567befb53c39d3e1121e10da3e41582bbe2c1a
Jakob Wagner <wobakjerna@web.de>
2016-06-21 16:24:45 +0200

    take only pixels behind average depth into account

--- glsl/tsdf_inpaint.fs
+++ glsl/tsdf_inpaint.fs
@@ -1,5 +1,5 @@
 #version 430
-#extension GL_ARB_shader_texture_lod : enable
+#extension GL_EXT_shader_texture_lod : enable
 noperspective in vec2 pass_TexCoord;
 
 uniform sampler2D texture_color;
@@ -9,37 +9,63 @@ uniform int lod;
 out vec4 out_FragColor;
 out float gl_FragDepth;
 
-const int kernel_size = 8;
+const int kernel_size = 4;
 
 const float gauss_weights[16] = {
-	0.4, 0.9, 0.9, 0.4,
- 	0.9, 1.8, 1.8, 0.9,
- 	0.9, 1.8, 1.8, 0.9,
-	0.4, 0.9, 0.9, 0.4
+  0.4, 0.9, 0.9, 0.4,
+  0.9, 1.8, 1.8, 0.9,
+  0.9, 1.8, 1.8, 0.9,
+  0.4, 0.9, 0.9, 0.4
 };
 
 void main() {
+  // vec4 total_color = vec4(0.0f);
+  // float total_depth = 0.0f;
+  // float total_weight = 0.0f;
+  vec4 samples[kernel_size * kernel_size];
+  for(int x = 0; x < kernel_size; ++x) {
+    for(int y = 0; y < kernel_size; ++y) {
+      const ivec2 pos = clamp(ivec2(pass_TexCoord * resolution_tex) + ivec2(x-4/2+1, y-4/2+1), ivec2(0), ivec2(resolution_tex)-1);
+      vec4 color = texelFetch(texture_color, pos, lod);
+      if (color == vec4(0.0f, 0.0f, 0.0f, 1.0f)) {
+        color.r = -1.0f;
+      }
+        float depth = texelFetch(texture_depth, pos, lod).r;
+        samples[x + y * kernel_size] = vec4(color.rgb, depth);
+        // float weight = gauss_weights[x + kernel_size * y];
+        // total_color += color * weight;
+        // total_depth += depth * weight;
+        // total_weight += weight;
+      // }
+    }
+  }
+
+  float depth_av = 0.0f;
+  int num_samples = 0;
+  for(int i = 0; i < kernel_size * kernel_size; ++i) {
+    if (samples[i].r >= 0.0f) {
+      depth_av += samples[i].a;
+        ++num_samples;
+    }
+  }
+  depth_av /= float(num_samples);
+  
   vec4 total_color = vec4(0.0f);
   float total_depth = 0.0f;
   float total_weight = 0.0f;
+  for(int i = 0; i < kernel_size * kernel_size; ++i) {
+    if (samples[i].r >= 0.0f) {
 
-  for(int x = 0; x < kernel_size; ++x) {
-  	for(int y = 0; y < kernel_size; ++y) {
-	 		const ivec2 pos = clamp(ivec2(pass_TexCoord * resolution_tex) + ivec2(x-4/2 + 1, y-4/2 + 1), ivec2(0), ivec2(resolution_tex)-1);
-      // vec4 color = textureLod(texture_color, (vec2(pos) - 0.5f)/ vec2(resolution_tex), 0);
-	 		vec4 color = texelFetch(texture_color, pos, lod);
-	 		if (color != vec4(0.0f, 0.0f, 0.0f, 1.0f)) {
-		 		float depth = texelFetch(texture_depth, pos, lod).r;
-        // float weight = gauss_weights[x + kernel_size * y];
-		 		float weight = 1.0f;
-		 		total_color += color * weight;
-		 		total_depth += depth * weight;
-		 		total_weight += weight;
-	 		}
-		}
+    if (samples[i].a > depth_av) {
+      float weight = gauss_weights[i];
+      total_color.rgb += samples[i].rgb * weight;
+      total_depth += samples[i].a * weight;
+      total_weight += weight;
+    }
   }
-
+  }
   out_FragColor = total_color / total_weight;
+  out_FragColor.w = 1.0f;
   if(total_weight <= 0.0f) out_FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
   gl_FragDepth = total_depth / total_weight;
 }
---
+++ glsl/tsdf_inpaint_2.fs
@@ -0,0 +1,45 @@
+#version 430
+#extension GL_ARB_shader_texture_lod : enable
+noperspective in vec2 pass_TexCoord;
+
+uniform sampler2D texture_color;
+uniform sampler2D texture_depth;
+uniform uvec2 resolution_tex;
+uniform int lod;
+out vec4 out_FragColor;
+out float gl_FragDepth;
+
+const int kernel_size = 8;
+
+const float gauss_weights[16] = {
+	0.4, 0.9, 0.9, 0.4,
+ 	0.9, 1.8, 1.8, 0.9,
+ 	0.9, 1.8, 1.8, 0.9,
+	0.4, 0.9, 0.9, 0.4
+};
+
+void main() {
+  vec4 total_color = vec4(0.0f);
+  float total_depth = 0.0f;
+  float total_weight = 0.0f;
+
+  for(int x = 0; x < kernel_size; ++x) {
+  	for(int y = 0; y < kernel_size; ++y) {
+	 		const ivec2 pos = clamp(ivec2(pass_TexCoord * resolution_tex) + ivec2(x-4/2 + 1, y-4/2 + 1), ivec2(0), ivec2(resolution_tex)-1);
+      // vec4 color = textureLod(texture_color, (vec2(pos) - 0.5f)/ vec2(resolution_tex), 0);
+	 		vec4 color = texelFetch(texture_color, pos, lod);
+	 		if (color != vec4(0.0f, 0.0f, 0.0f, 1.0f)) {
+		 		float depth = texelFetch(texture_depth, pos, lod).r;
+        // float weight = gauss_weights[x + kernel_size * y];
+		 		float weight = 1.0f;
+		 		total_color += color * weight;
+		 		total_depth += depth * weight;
+		 		total_weight += weight;
+	 		}
+		}
+  }
+
+  out_FragColor = total_color / total_weight;
+  if(total_weight <= 0.0f) out_FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+  gl_FragDepth = total_depth / total_weight;
+}
